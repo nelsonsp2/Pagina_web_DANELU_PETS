c6d49aca76a4845bd192b4b4f87a9c08127f80c2
nelsonsp2 <nelsonguayazansp@gmail.com>
2020-11-07 13:27:24 -0500

    nelsiton

--- blog/models.py
+++ blog/models.py
@@ -54,3 +54,51 @@ class Cliente(models.Model):
     def __str__(self):
         return self.id_cliente
 
+class Carrito(models.Model):
+    id_carrito = models.CharField(max_length=200, primary_key= True)
+    id_producto = models.CharField(max_length=200, primary_key=True)
+    cantidad_producto = models.SmallIntegerField()
+    total_producto = models.IntegerField()
+    id_cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('-id_carrito', '-id_producto')
+
+    def __str__(self):
+        return self.id_carrito
+
+class Cuenta(models.Model):
+    numero_cuenta = models.CharField(max_length=200, primary_key= True)
+    valor_total = models.IntegerField()
+    id_carrito = models.ForeignKey(Carrito, on_delete= models.CASCADE)
+    class Meta:
+        ordering = ('-numero_cuenta', '-valor_total')
+
+    def __str__(self):
+        return self.id_carrito
+class Domiciliario(models.Model):
+    id_domicilario = models.CharField(max_length=200, primary_key= True)
+    Nombre1 = models.CharField(max_length=50)
+    Nombre2 = models.CharField(max_length=50)
+    Apellido1 = models.CharField(max_length=50)
+    Apellido2 = models.CharField(max_length=50)
+    Medio_transporte = models.CharField(max_length=50)
+
+    class Meta:
+        ordering = ('-id_domicilario', '-Medio_transporte')
+
+    def __str__(self):
+        return self.id_domicilario
+
+class Domicilio(models.Model):
+    id_domicilio = models.CharField(max_length=200, primary_key=True)
+    id_domiciliario = models.ForeignKey(Domiciliario,on_delete=models.CASCADE)
+    direccion = models.CharField(max_length=100)
+    numero_cuenta = models.ForeignKey(Cuenta,on_delete=models.CASCADE)
+    metodo_de_pago = models.CharField(max_length=50)
+
+    class Meta:
+        ordering=('-id_domicilio','-id_domiciliario')
+
+    def __str__(self):
+        return self.id_domicilio
