dadd7dbcaf93a7489f503232a4fff46654acada6
Biblioteca UR <bibliotecaur@urosario.edu.co>
2020-11-20 16:20:13 -0500

    bookmarks con sistema de autenticacion

---
+++ bookmarks/account/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    # path('login/', views.user_login, name='login')
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', views.dashboad, name="dashboard"),
+
+    # rutas de cambio de contraseña
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name="password_change"),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name="password_change_done"),
+]
