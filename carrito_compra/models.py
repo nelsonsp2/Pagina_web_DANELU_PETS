4ed527222f493fbe8df7ac4fb6da018f47619c36
Biblioteca UR <bibliotecaur@urosario.edu.co>
2020-12-04 00:38:53 -0500

    alertas en la agregacion de productos

--- carrito_compra/models.py
+++ carrito_compra/models.py
@@ -2,10 +2,6 @@ from django.db import models
 from django.conf import settings
 from django.shortcuts import reverse
 from django.contrib.auth.models import User
-from django.contrib import auth
-#from django. import
-# Create your models here.
-#import authentication
 
 
 class Categoria(models.Model):
